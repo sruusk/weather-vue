642894521226e6dc937bd5c006b4154157abc0ae
Samuli Ruuskanen <samuli.ruuskanen@live.fi>
2023-02-14 21:00:47 +0000

    mkdir

--- .space.kts
+++ .space.kts
@@ -39,6 +39,7 @@ job("Deploy") {
                 echo Run build ...
                 npm run build
                 echo Deploying...
+                mkdir ~/.ssh
                 echo ${'$'}SSHKEY > ~/.ssh/id_rsa
                 scp -i ~/.ssh/id_rsa ./dist/* weather@weather.a32.fi:/opt/www/weather/
                 echo Deployment complete!
