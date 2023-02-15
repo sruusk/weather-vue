d2d978500abff76304a56b6a4cfa9995ac0cfc22
Samuli Ruuskanen <samuli.ruuskanen@live.fi>
2023-02-15 00:27:30 +0000

    Add Qodana code analysis

--- .space.kts
+++ .space.kts
@@ -4,6 +4,20 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
+job("Qodana") {
+   container("jetbrains/qodana-js") {
+      env["QODANA_TOKEN"] = Secrets("qodana-token")
+      shellScript {
+         content = """
+            QODANA_REMOTE_URL="ssh://git@git.${'$'}JB_SPACE_API_URL/${'$'}JB_SPACE_PROJECT_KEY/${'$'}JB_SPACE_GIT_REPOSITORY_NAME.git" \
+            QODANA_BRANCH=${'$'}JB_SPACE_GIT_BRANCH \
+            QODANA_REVISION=${'$'}JB_SPACE_GIT_REVISION \
+            qodana
+         """.trimIndent()
+      }
+   }
+}
+
 job("Deploy") {
     // run on commit to a branch containing "master"
     startOn {
