66830e5053a35ebcb71ccc7ea62066b6d5a5ff81
Samuli Ruuskanen <samuli.ruuskanen@live.fi>
2023-02-15 00:43:11 +0000

    npm ci

--- .space.kts
+++ .space.kts
@@ -9,6 +9,7 @@ job("Qodana") {
       env["QODANA_TOKEN"] = Secrets("qodana-token")
       shellScript {
          content = """
+         	npm ci
             QODANA_REMOTE_URL="ssh://git@git.${'$'}JB_SPACE_API_URL/${'$'}JB_SPACE_PROJECT_KEY/${'$'}JB_SPACE_GIT_REPOSITORY_NAME.git" \
             QODANA_BRANCH=${'$'}JB_SPACE_GIT_BRANCH \
             QODANA_REVISION=${'$'}JB_SPACE_GIT_REVISION \
