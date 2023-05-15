8b9ba82b42804e104a9f7d37cb07f1e7fdaccb89
Samuli Ruuskanen <samuli.ruuskanen@tuni.fi>
2023-05-16 02:10:08 +0300

    Make horizon icon static to fix performance issue

--- src/components/icons/SunIcon.vue
+++ src/components/icons/SunIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg v-if="animate" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <defs>
             <linearGradient id="a" gradientUnits="userSpaceOnUse" x1="150" x2="234" y1="119.2" y2="264.8">
                 <stop offset="0" stop-color="#fbbf24"/>
@@ -25,4 +25,41 @@
         <path d="M100 332h300" fill="none" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round"
               stroke-width="18"/>
     </svg>
+
+
+    <svg v-else xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
+        <defs>
+            <linearGradient id="a" x1="149.99" y1="119.24" x2="234.01" y2="264.76" gradientUnits="userSpaceOnUse">
+                <stop offset="0" stop-color="#fbbf24"/>
+                <stop offset="0.45" stop-color="#fbbf24"/>
+                <stop offset="1" stop-color="#f59e0b"/>
+            </linearGradient>
+            <clipPath id="b">
+                <rect width="512" height="306" fill="none"/>
+            </clipPath>
+            <symbol id="c" viewBox="0 0 384 384">
+                <circle cx="192" cy="192" r="84" stroke="#f8af18" stroke-miterlimit="10" stroke-width="6" fill="url(#a)"/>
+                <path d="M192,61.66V12m0,360V322.34M284.17,99.83l35.11-35.11M64.72,319.28l35.11-35.11m0-184.34L64.72,64.72M319.28,319.28l-35.11-35.11M61.66,192H12m360,0H322.34" fill="none" stroke="#fbbf24" stroke-linecap="round" stroke-miterlimit="10" stroke-width="24"/>
+            </symbol>
+        </defs>
+        <g clip-path="url(#b)">
+            <use width="384" height="384" transform="translate(64 100)" xlink:href="#c"/>
+        </g>
+        <line x1="110" y1="332" x2="400" y2="332" fill="none" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="18"/>
+    </svg>
+
 </template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue';
+
+    export default defineComponent({
+        name: "BasicIcon.vue",
+        props: {
+            animate: {
+                type: Boolean,
+                default: false
+            }
+        }
+    })
+</script>
